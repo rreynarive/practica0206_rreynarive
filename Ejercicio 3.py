3c86c58da4c9c976ef565e64471dd5eb9eef5f29
Rosa <rreynarive1@educacion.navarra.es>
2022-11-09 20:26:43 +0100

    Modificacion ejer2

---
+++ Ejercicio 3.py
@@ -0,0 +1,5 @@
+# Escribir un programa que guarde en un diccionario
+# los precios de los artículos de la tabla, pregunte
+# al usuario por un artículo, un número de unidades y muestre
+# por pantalla el precio de esa cantidad de producto.
+# Si el producto no está en el diccionario debe mostrar un mensaje informando de ello.

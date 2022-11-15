17a846ef3fb6652633a8bc405fc4227e07146ded
Rosa <rreynarive1@educacion.navarra.es>
2022-11-15 16:01:23 +0100

    Ejercicio 3 FIN

--- Ejercicio 3.py
+++ Ejercicio 3.py
@@ -3,3 +3,14 @@
 # al usuario por un artículo, un número de unidades y muestre
 # por pantalla el precio de esa cantidad de producto.
 # Si el producto no está en el diccionario debe mostrar un mensaje informando de ello.
+precio = {"Pan": 1.40, "Huevos": 2.30, "Cebolla": 0.85, "Aceite": 4.35}
+
+articulo = input("Introduce el articulo que quiere comprar:\n")
+articulo = articulo.title()
+cantidad = int(input("Introduce la cantidad:\n"))
+
+if articulo in precio.keys():
+    print(cantidad, "unidades de ", articulo, "son: ",
+          cantidad * precio[articulo], "€")
+else:
+    print("Este articulo no pertenece al diccionario")

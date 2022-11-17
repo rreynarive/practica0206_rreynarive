60b1b477a0e8de82e14e62d3794e093b7489780a
Rosa Reyna <rreynarive1@educacion.navarra.es>
2022-11-17 02:06:05 +0100

    ejer 6 falta

--- Ejercicio 6.py
+++ Ejercicio 6.py
@@ -1,2 +1,12 @@
-datos_alumnos = {}
+clase = [{"73464267j": {"Alumno1": {"Nombre": "Rosa", "Apellido": "Reyna", "Telefono": "123456789", "Correo": "Reynarive.educacion.es", "Aprobado": True}}},
+         {"12345678m": {"Alumnno2": {"Nombre": "Jos", "Apellido": "Aba", "Telefono": "125478964", "Correo": "abajos.educacion.es", "Aprobado": False}}}]
+
+
+menu = "Eliga la siguente accion:\n"\
+       "(1)- Añadir un alumno/a\n"\
+       "(2)- Eliminar un alumno/a\n"\
+       "(3)- Mostrar datos de un alumno/a\n"\
+       "(4)- Listar todo el alumnado\n"\
+       "(5)- Listar alumnados aprobados\n"\
+       "(6)- Terminar\n"
 
